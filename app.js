644c56b734c9672c154f50143073a8dd1050a52f
Thossapol <76428440+thossapolud@users.noreply.github.com>
2021-03-15 02:45:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var app = require('express')();
-var http = require('htpp').Server(app);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (req,res) {
-    console.log('connected')
+    console.log('connected');
+    res.sendStatus(200);
 });
 
-http.lesten(3000,function () {
+io.on('connection', function(socket) {
+    console.log('A user connected');
+
+    socket.on('disconected' , function(){
+        console.log('A user disconected');
+    });
+});
+
+http.listen(3000,function () {
     console.log('listening on port 3000');
 });
